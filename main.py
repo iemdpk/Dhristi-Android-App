16705396319723a14f9d0385dcfb1384a65c5422
Deepak Mishra <40403460+Devils-Hunter@users.noreply.github.com>
2019-11-07 23:25:35 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -4,8 +4,20 @@ from kivy.lang import Builder
 from kivy.core.window import Window
 from kivy.utils import get_color_from_hex
 from kivy.uix.gridlayout import GridLayout
+from kivy.properties import ObjectProperty
+
 
 class Deepak(GridLayout):
+
+	name = ObjectProperty(None)
+	email1 = ObjectProperty(None)
+	number1 = ObjectProperty(None)
+	orga1 = ObjectProperty(None)
+
+	def btn(self):
+		print("Name : " , self.name.text,"email : " , self.email.text,"Number : " , self.number.text,"Organisation : " , self.orga.text)
+		
+
 	pass
 		
 
